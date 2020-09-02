891164a259c7b0dbabf3074234351028d6b05343
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-09-02 20:36:20 +0000

    Deploy Willlll87/demo-app to github.com/Willlll87/demo-app.git:gh-pages

---
+++ precache-manifest.22a0ad13e5a34bfb1d8a96c341762173.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "142cf5e9fd9056d40536",
+    "url": "/demo-app/static/css/main.6b29ab88.chunk.css"
+  },
+  {
+    "revision": "142cf5e9fd9056d40536",
+    "url": "/demo-app/static/js/main.142cf5e9.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/demo-app/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "28fc3e86c9ea21c389e9",
+    "url": "/demo-app/static/js/runtime~main.28fc3e86.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/demo-app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e9ce845c2e3774679ae61e3dd007a6b5",
+    "url": "/demo-app/index.html"
+  }
+];
